3d7dd6efcf5d845e1e47ce020122ff0cd5493850
RobertJ <rockfade@gmail.com>
2025-12-05 19:54:29 +0100

    add routing to presentation rest api

--- app/app/config/router.php
+++ app/app/config/router.php
@@ -2,9 +2,19 @@
 
 declare(strict_types=1);
 
+
 $router = $di->getRouter();
 
 // Define your routes here
+
+$router->add(
+    '/api/:controller/:action',
+    [
+        'namespace'  => 'AppDomain\Presentation\Api',
+        'controller' => 1,
+        'action'     => 2,
+    ]
+);
 
 $router->setDefaultNamespace('App\Controllers');
 
